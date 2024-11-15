af8f9577c5161c30016a31d54f796cf38289f440
SachinRuhela1922 <156918990+SachinRuhela1922@users.noreply.github.com>
2024-11-15 02:24:21 -0800

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const port = 5000;
 
 app.use(cors(
     {
-        origin:{"https://holomad-api.vercel.app"},
+        origin:{""},
         methods:{"POST","GET"},
         credentials:true
     }
